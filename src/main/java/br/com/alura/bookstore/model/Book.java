30112c1b990ee47f1999c921172ae110b55bd2ce
enlima <everton.lima@live.com>
2021-09-24 19:04:45 -0300

    style: formatting

--- src/main/java/br/com/alura/bookstore/model/Book.java
+++ src/main/java/br/com/alura/bookstore/model/Book.java
@@ -1,6 +1,9 @@
 package br.com.alura.bookstore.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
