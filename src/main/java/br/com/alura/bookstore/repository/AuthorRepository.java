6e827e50c80f9b3388792c97f0591bf8d7cd7e84
enlima <everton.lima@live.com>
2021-10-05 20:22:04 -0300

    create repository package

---
+++ src/main/java/br/com/alura/bookstore/repository/AuthorRepository.java
@@ -0,0 +1,8 @@
+package br.com.alura.bookstore.repository;
+
+import br.com.alura.bookstore.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+}
---
+++ src/main/java/br/com/alura/bookstore/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package br.com.alura.bookstore.repository;
+
+import br.com.alura.bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
