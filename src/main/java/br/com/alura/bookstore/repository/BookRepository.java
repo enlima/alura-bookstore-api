c0c906c01badb41dacf8e637033f3ca6d8e5a051
enlima <everton.lima@live.com>
2021-10-09 12:58:06 -0300

    format code

--- src/main/java/br/com/alura/bookstore/repository/BookRepository.java
+++ src/main/java/br/com/alura/bookstore/repository/BookRepository.java
@@ -9,7 +9,8 @@ import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    @Query("SELECT new br.com.alura.bookstore.dto.ItemBookstoreDto (a.name, COUNT(b.id), ROUND((COUNT(b.id) * 100.0) / (SELECT COUNT(b2.id) FROM Book b2), 2)) "
+    @Query("SELECT new br.com.alura.bookstore.dto.ItemBookstoreDto (a.name, COUNT(b.id), "
+            + "ROUND((COUNT(b.id) * 100.0) / (SELECT COUNT(b2.id) FROM Book b2), 2)) "
             + "FROM Book b JOIN b.author a GROUP BY b.author")
     List<ItemBookstoreDto> reportBooksByAuthors();
 }
